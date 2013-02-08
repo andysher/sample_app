b03bd9666d8da1e1bf2d716b4dc71fcd4a92ca90
andysher <sherkar.aniruddha@gmail.com>
2013-02-09 01:04:17 +0530

    Make a basic User model (including secure password git add .! Fhewgit add .)

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@
 #  updated_at      :datetime         not null
 #  password_digest :string(255)
 #
+#
 
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
