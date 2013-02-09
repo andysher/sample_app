920094d4d2bde8f5e9fbc16ec1b3302ab655606d
andysher <sherkar.aniruddha@gmail.com>
2013-02-09 20:22:54 +0530

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name "Aniruddha Sherkar"
+    email "sherkar.aniruddha@gmail.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
